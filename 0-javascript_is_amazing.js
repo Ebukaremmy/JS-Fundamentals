380c11485285adb3ca1485ce9161b33a00bfc201
ebukaremmy <ebukaremmyikegwuonu@gmail.com>
2025-11-07 04:47:31 -0800

    Add README and first JS script

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,4 @@
+// This script prints “JavaScript is amazing”
 const myVar = "JavaScript is amazing";
-console.log(myVar); 
+console.log(myVar);
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-Fundamentals
+This repository contains my JavaScript fundamentals tasks for ALX.
